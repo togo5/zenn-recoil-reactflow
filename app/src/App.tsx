90d5ca7c4e82f17f9816ce57ee204ca55a36b1e0
Hiroki Goto <hiroki.gotoh@outlook.com>
2024-03-23 17:57:17 +0000

    ✨ feat: Add react flow and recoil

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ReactFlow, { Background, Controls } from 'reactflow'
+import 'reactflow/dist/style.css'
+import { useFlowStore } from './states'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { nodes, edges, onConnect, onNodesChange, onEdgesChange } = useFlowStore()
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ height: '100vh', width: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onConnect={onConnect}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
   )
 }
 
---
+++ app/src/states.ts
@@ -0,0 +1,48 @@
+import { Connection, Edge, EdgeChange, Node, NodeChange, addEdge, applyEdgeChanges, applyNodeChanges } from 'reactflow'
+import { atom, useRecoilCallback, useRecoilValue } from 'recoil'
+
+const nodesState = atom<Node[]>({
+  key: 'states.nodesState',
+  default: [
+    {
+      id: '1',
+      data: { label: 'Hello' },
+      position: { x: 0, y: 0 },
+      type: 'input',
+    },
+    {
+      id: '2',
+      data: { label: 'World' },
+      position: { x: 100, y: 100 },
+    },
+  ],
+})
+
+const edgesState = atom<Edge[]>({
+  key: 'states.edgesState',
+  default: [{ id: '1-2', source: '1', target: '2', label: 'to the', type: 'step' }],
+})
+
+export const useFlowStore = () => {
+  return {
+    nodes: useRecoilValue(nodesState),
+    edges: useRecoilValue(edgesState),
+    onConnect: useRecoilCallback(({ set }) => (connection: Connection) => {
+      set(edgesState, (edges) => addEdge(connection, edges))
+    }),
+    onNodesChange: useRecoilCallback(
+      ({ set }) =>
+        (changes: NodeChange[]) => {
+          set(nodesState, (nodes) => applyNodeChanges(changes, nodes))
+        },
+      []
+    ),
+    onEdgesChange: useRecoilCallback(
+      ({ set }) =>
+        (changes: EdgeChange[]) => {
+          set(edgesState, (edges) => applyEdgeChanges(changes, edges))
+        },
+      []
+    ),
+  }
+}
